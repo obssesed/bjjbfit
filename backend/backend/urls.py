427e97e9e086aabee5af03a431963f43a0df0922
Obssesed <vgonli1992@gmail.com>
2026-07-13 20:45:44 +0200

    fix: force django to serve media files in production

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -39,13 +39,16 @@ router.register(r'solicitudes-reseteo', SolicitudReseteoViewSet, basename='solic
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
